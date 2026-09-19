1479f873be68eb98c03dd71f8dad3c336c3146b7
Andrii Pasichnyk <67899666+pasichDev@users.noreply.github.com>
2026-09-19 11:23:33 +0300

    privacy: disable IME learning for task content

--- app/src/main/java/com/pasich/encly/presentation/dialogs/tasks/AddTaskDialog.kt
+++ app/src/main/java/com/pasich/encly/presentation/dialogs/tasks/AddTaskDialog.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -31,6 +32,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.pasich.encly.data.model.Task
 import com.pasich.encly.presentation.components.tasks.PriorityIndicator
@@ -210,6 +212,7 @@ private fun TaskTextField(
         },
         modifier = modifier.padding(0.dp),
         singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.PasswordVisible),
         textStyle = if (bodyStyle) MaterialTheme.typography.bodyMedium
         else MaterialTheme.typography.bodyLarge,
         colors = TextFieldDefaults.colors(
